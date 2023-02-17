03b3b45007d93c9faee33b159e323ab0f78dd8b6
joeyzhou <zhouyang0715@yahoo.com>
2023-02-17 16:56:41 -0500

    changed sounds file

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ class Game {
         this.moves = 0;
         this.seconds = 100;
         this.flips = 1;
-        this.bgmusic = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/Run-Amok.mp3');
-        this.clickaudio = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/video-game-mystery-alert.wav');
-        this.startaudio = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/mixkit-game-click-1114 (1).wav');
-        this.mathchsound = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/mixkit-arcade-mechanical-bling-210.wav');
-        this.gamoversound = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/mixkit-sad-game-over-trombone-471.wav');
-        this.winsound = new Audio('/Users/joeyzhou/Desktop/seirfx-eustoma/projects/project1/sounds/mixkit-animated-small-group-applause-523.wav');
+        this.bgmusic = new Audio('./sounds/Run-Amok.mp3');
+        this.clickaudio = new Audio('./sounds/video-game-mystery-alert.wav');
+        this.startaudio = new Audio('./sounds/mixkit-game-click-1114 (1).wav');
+        this.mathchsound = new Audio('./sounds/mixkit-arcade-mechanical-bling-210.wav');
+        this.gamoversound = new Audio('./sounds/mixkit-sad-game-over-trombone-471.wav');
+        this.winsound = new Audio('./sounds/mixkit-animated-small-group-applause-523.wav');
         this.bgmusic.volume = 0.2;
         this.bgmusic.loop = true;
     }
